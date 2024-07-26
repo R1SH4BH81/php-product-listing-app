d29dfcd322861b1ca236d9afee2697675dca6398
RISHABH MISHRA <92362688+R1SH4BH81@users.noreply.github.com>
2024-07-27 01:43:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
-// vite.config.js
+// client/vite.config.js
 
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
+  plugins: [react(),
+           laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
-        }),
-    ],
+        }),],
+  build: {
+    outDir: 'dist', // Output directory for build files
+  },
+  server: {
+    port: 3000,
+  },
 });
